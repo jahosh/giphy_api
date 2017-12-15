8ac922f80589d76c6b1195a98ee588f1306920e5
=josh91hickman <josh91hickman@gmail.com>
2017-12-15 09:23:36 -0800

    initial commit

---
+++ src/containers/AppContainer/AppContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './AppContainer.css';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Giphy Api</h1>
+        </header>
+      
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
